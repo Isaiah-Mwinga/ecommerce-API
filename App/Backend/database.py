b238bc01a5ac8f6b150d15aefbda8e4302544443
Isaiah-Mwinga <motongori@marketforce360.com>
2022-10-31 12:54:10 +0300

    db setup

--- App/Backend/database.py
+++ App/Backend/database.py
@@ -2,4 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@postgresserver/testdb"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
