a8bad13a9753027237471b1edd6e6430f3248d56
Isaiah-Mwinga <motongori@marketforce360.com>
2022-11-06 22:09:03 +0300

    Auth2 schemes

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from .database import SessionLocal, engine
 
 from . import models, schemas
+from .hashing import Hash
+from . import token
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -32,5 +36,5 @@ def get_db():
         db.close()
 
 @app.get("/Items")
-def read_items():
-    return [{"item_id": "Foo"}]
+def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
