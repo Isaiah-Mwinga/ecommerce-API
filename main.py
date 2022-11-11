ffa20474c25073fdea3b145d19870accd409bf14
Isaiah-Mwinga <motongori@marketforce360.com>
2022-11-11 21:35:07 +0300

    modified

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
