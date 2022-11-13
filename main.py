d9bbeb14286ad6dd8cc6d2710b75874771767e29
Isaiah-Mwinga <motongori@marketforce360.com>
2022-11-13 11:12:48 +0300

    add the auth scheme

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Dependency
 def get_db():
@@ -14,5 +16,5 @@ def get_db():
         db.close()
 
 @app.get("/Items")
-def read_root():
+def read_root(token: str = Depends(oauth2_scheme)):
     return {"Hello": "World"}
