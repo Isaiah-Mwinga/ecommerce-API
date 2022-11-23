5f418c6f3073befcc8d1d887727159a7524b8d9f
Isaiah-Mwinga <motongori@marketforce360.com>
2022-11-23 10:48:04 +0300

    db setup

--- app/database.py
+++ app/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@postgresserver/testdb"
+DATABASE_URL = "postgresql://postgres:root@postgresserver/testdb"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    DATABASE_URL
 )
 Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from app.models import categories
+from app.schemas import User, Item
+from fastapi_sqlalchemy import DBSessionMiddleware, db
+
+from app.models import User, Item
+from app.database import Sessionlocal, engine
+
+import os
+from dotenv import load_dotenv
+load_dotenv('.env.local')
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+ #to avoid csrftokenError
+app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
+
 
 # Dependency
 def get_db():
