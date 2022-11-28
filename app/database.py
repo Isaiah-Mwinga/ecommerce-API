5383b74c99dfeef1906d1515fa85261c80e18774
Isaiah-Mwinga <motongori@marketforce360.com>
2022-11-28 19:59:14 +0300

    db

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@postgresserver/testdb"
+DATABASE_URL = "postgresql://postgres:root@localhost/testdb"
 
 engine = create_engine(
     DATABASE_URL
