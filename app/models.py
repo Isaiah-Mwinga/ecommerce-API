ab73e29eaa884e58953021cc263fc70c26878136
Isaiah-Mwinga <motongori@marketforce360.com>
2022-12-20 16:21:16 +0300

    minor refactor

--- app/models.py
+++ app/models.py
@@ -4,11 +4,11 @@ from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    is_active = Column(Boolean, default=True)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, index=True, unique=True, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
 
     items = relationship("Item", back_populates="owner")
 
@@ -17,11 +17,12 @@ class User(Base):
 
 class Item(Base):
     __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    title = Column(String, index=True, nullable=False)
+    description = Column(String, index=True, nullable=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
-    price = Column(Integer , index=True)
+    price = Column(Integer , index=True, nullable=False)
+    tax = Column(Integer , index=True, nullable=False)
 
     owner = relationship("User", back_populates="items")    
 
