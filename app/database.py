173b164472a85c7490dd96ce155966ad2c905351
Isaiah-Mwinga <motongori@marketforce360.com>
2022-12-21 08:25:28 +0300

    cchnanged anged the DB

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL= "postgresql+psycopg2://postgres:root@localhost/testdb"
+DATABASE_URL= "postgresql+psycopg2://postgres:root@localhost/Item_db"
 engine = create_engine(DATABASE_URL)
                          
     
