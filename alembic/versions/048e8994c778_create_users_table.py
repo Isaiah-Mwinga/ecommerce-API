4b4333473be225d7c4ffdecf078c8cf96fb93d5c
Isaiah-Mwinga <motongori@marketforce360.com>
2022-12-21 08:47:50 +0300

    first migration

---
+++ alembic/versions/048e8994c778_create_users_table.py
@@ -0,0 +1,24 @@
+"""create users table
+
+Revision ID: 048e8994c778
+Revises: 
+Create Date: 2022-12-21 08:46:38.475783
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '048e8994c778'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
