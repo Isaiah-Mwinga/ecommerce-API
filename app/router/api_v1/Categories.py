a4b36beb9a4e4af44936b68624a9b2211245fb0e
Isaiah-Mwinga <motongori@marketforce360.com>
2022-12-30 21:43:14 +0300

    categories route

--- app/router/api_v1/Categories.py
+++ app/router/api_v1/Categories.py
@@ -2,3 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.schemas import categories
+
+router = APIRouter(
+    prefix="Categories",
+    tags=["Categories"],
+    # dependencies=[Depends(get_token_header)],
+)
