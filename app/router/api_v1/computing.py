148dba6692393a28e084be8f68ccd04a20452da1
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-01 09:29:17 +0300

    adding the router

--- app/router/api_v1/computing.py
+++ app/router/api_v1/computing.py
@@ -2,3 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.schemas import Computing 
+
+router = APIRouter(
+    prefix="/Computing",
+    tags=["Computing"],
+    # dependencies=[Depends(get_token_header)],
+)
