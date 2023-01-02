95f2129ecbeabd5410a4ea9556b4d7a1884f88a7
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-02 12:44:47 +0300

    get computing items by ID

--- app/router/api_v1/Computing.py
+++ app/router/api_v1/Computing.py
@@ -20,4 +20,10 @@ def get_db():
 @router.get("/")
 def read_computing(db: Session = Depends(get_db)):
     return db.query(Computing).all()
-    
+
+@router.get("/{computing_id}")
+def read_computing(computing_id: int, db: Session = Depends(get_db)):
+    db_computing = db.query(Computing).filter(Computing.id == computing_id).first()
+    if db_computing is None:
+        raise HTTPException(status_code=404, detail="Computing not found")
+    return db_computing
