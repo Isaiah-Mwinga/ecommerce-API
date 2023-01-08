98a82656bed55bc4ea81096c7603edd1b3f3d262
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-08 13:31:15 +0300

    adding jwt tokens

--- app/router/api_v1/Users.py
+++ app/router/api_v1/Users.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from app.schemas import User
 from app import models, schemas, crud
+
+
+from app.crud import (
+    Token,
+    get_current_active_user,
+    get_current_user,
+    authenticate_user,
+    create_access_token,
+    get_user,
+    )
 
 router = APIRouter(
     prefix="/users",
@@ -61,4 +73,20 @@ def delete_User(user_id: int , db: Session = Depends(get_db)):
     db.delete(db_user)
     db.commit()
     return db_user
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
 
