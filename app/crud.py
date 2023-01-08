b8809cc77458519af744a01293eeb5a0f3c70174
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-08 22:45:16 +0300

    deleted Item endpoint

--- app/crud.py
+++ app/crud.py
@@ -31,13 +31,13 @@ def create_user(db: Session, user: schemas.User):
     return db_user
 
 
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+#def get_items(db: Session, skip: int = 0, limit: int = 100):
+#    return db.query(models.Item).offset(skip).limit(limit).all()
 
 
-def create_user_item(db: Session, item: schemas.Item, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
+#def create_user_item(db: Session, item: schemas.Item, user_id: int):
+#    db_item = models.Item(**item.dict(), owner_id=user_id)
+#    db.add(db_item)
+#    db.commit()
+#    db.refresh(db_item)
+#    return db_item
