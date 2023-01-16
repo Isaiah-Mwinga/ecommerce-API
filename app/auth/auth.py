7a33e6af52446384e355d2924423166f46fe63d6
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-16 20:54:43 +0300

    handling auth with jwt

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -16,16 +16,27 @@ router = APIRouter(
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/login", response_model=schemas.Token)
-def login(form: OAuth2PasswordRequestForm = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    user = services.authenticate_user(email=form.username, password=form.password, db=db)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = services.create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+#def login(form: OAuth2PasswordRequestForm = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+#    user = services.authenticate_user(email=form.username, password=form.password, db=db)
+#    if not user:
+#        raise HTTPException(
+#            status_code=status.HTTP_401_UNAUTHORIZED,
+#            detail="Incorrect username or password",
+#            headers={"WWW-Authenticate": "Bearer"},
+#        )
+#    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#    access_token = services.create_access_token(
+#        data={"sub": user.username}, expires_delta=access_token_expires
+#    )
+#    return {"access_token": access_token, "token_type": "bearer"}
