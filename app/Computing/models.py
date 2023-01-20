6b2c30f8146ba2bc5b297a915aecc7e80dd53491
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-20 21:11:50 +0300

    creating models

--- app/Computing/models.py
+++ app/Computing/models.py
@@ -23,5 +23,65 @@ class Computing(Base):
             description = Column(String, index=True)
 
             def __repr__(self):
-                return f"Laptops(id={self.id}, name={self.name}, category_id={self.category_id}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
+                return f"Laptops(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
+
+        class Desktops(Computing):
+            __tablename__ = "desktops"
+            id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
+            brand = Column(String, index=True)
+            model = Column(String, index=True)
+            price = Column(Integer, index=True)
+            quantity = Column(Integer, index=True)
+            description = Column(String, index=True)
+
+            def __repr__(self):
+                return f"Desktops(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
+
+                class Datastorage(Computing):
+                    __tablename__ = "datastorage"
+                    id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
+                    brand = Column(String, index=True)
+                    model = Column(String, index=True)
+                    price = Column(Integer, index=True)
+                    quantity = Column(Integer, index=True)
+                    description = Column(String, index=True)
+
+                    def __repr__(self):
+                        return f"Datastorage(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
+
+                        class Monitors(Computing):
+                            __tablename__ = "monitors"
+                            id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
+                            brand = Column(String, index=True)
+                            model = Column(String, index=True)
+                            price = Column(Integer, index=True)
+                            quantity = Column(Integer, index=True)
+                            description = Column(String, index=True)
+
+                            def __repr__(self):
+                                return f"Monitors(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
+
+                                class Printers(Computing):
+                                    __tablename__ = "printers"
+                                    id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
+                                    brand = Column(String, index=True)
+                                    model = Column(String, index=True)
+                                    price = Column(Integer, index=True)
+                                    quantity = Column(Integer, index=True)
+                                    description = Column(String, index=True)
+
+                                    def __repr__(self):
+                                        return f"Printers(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
+
+                                        class Accessories(Computing):
+                                            __tablename__ = "accessories"
+                                            id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
+                                            brand = Column(String, index=True)
+                                            model = Column(String, index=True)
+                                            price = Column(Integer, index=True)
+                                            quantity = Column(Integer, index=True)
+                                            description = Column(String, index=True)
+
+                                            def __repr__(self):
+                                                return f"Accessories(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description})"
 
