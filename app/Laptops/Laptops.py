fcf322805e6e0293b78ec7a2292e7ae4a7960f47
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-24 22:58:03 +0300

    put and delete methods

--- app/Laptops/Laptops.py
+++ app/Laptops/Laptops.py
@@ -22,3 +22,17 @@ def get_Laptops_by_id(Laptops_id: int, db: Session = Depends(get_db)):
     if not db_Laptops:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Laptops not found")
     return db_Laptops
+
+@router.put("/Laptops/{Laptops_id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Laptops)
+def update_Laptops(Laptops_id: int, Laptops: schemas.Laptops, db: Session = Depends(get_db)):
+    db_Laptops = crud.get_Laptops_by_id(Laptops_id, db)
+    if not db_Laptops:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Laptops not found")
+    return crud.update_Laptops(Laptops_id, Laptops, db)
+
+@router.delete("/Laptops/{Laptops_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_Laptops(Laptops_id: int, db: Session = Depends(get_db)):
+    db_Laptops = crud.get_Laptops_by_id(Laptops_id, db)
+    if not db_Laptops:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Laptops not found")
+    crud.delete_Laptops(Laptops_id, db)
