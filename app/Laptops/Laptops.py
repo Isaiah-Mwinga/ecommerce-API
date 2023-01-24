a3b3dc8add6589491a224547d61b830ecc0f78e8
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-24 22:56:29 +0300

    app/Get method

--- app/Laptops/Laptops.py
+++ app/Laptops/Laptops.py
@@ -11,3 +11,14 @@ def create_Laptops(Laptops: schemas.Laptops, db: Session = Depends(get_db)):
     if crud.get_Laptops_by_name(Laptops.name, db):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Laptops already exists")
     return crud.create_Laptops(Laptops, db)
+
+@router.get("/Laptops/all", status_code=status.HTTP_200_OK, response_model=list[schemas.Laptops])
+def get_all_Laptops(db: Session = Depends(get_db)):
+    return crud.get_all_Laptops(db)
+
+@router.get("/Laptops/{Laptops_id}", status_code=status.HTTP_200_OK, response_model=schemas.Laptops)
+def get_Laptops_by_id(Laptops_id: int, db: Session = Depends(get_db)):
+    db_Laptops = crud.get_Laptops_by_id(Laptops_id, db)
+    if not db_Laptops:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Laptops not found")
+    return db_Laptops
