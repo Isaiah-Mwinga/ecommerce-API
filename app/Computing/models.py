c601f83327bda77d535dc618b0b245cdb56cc73f
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-26 23:10:10 +0300

    ident

--- app/Computing/models.py
+++ app/Computing/models.py
@@ -47,19 +47,17 @@ class Datastorage(Computing):
     image = Column(String, index=True)
     def __repr__(self):
         return f"Datastorage(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description}, image={self.image})"
-                        class Monitors(Computing):
-                            __tablename__ = "monitors"
-                            id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
-                            brand = Column(String, index=True)
-                            model = Column(String, index=True)
-                            price = Column(Integer, index=True)
-                            quantity = Column(Integer, index=True)
-                            description = Column(String, index=True)
-                            image = Column(String, index=True)
-
-                            def __repr__(self):
-                                return f"Monitors(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description}, image={self.image})"
-
+class Monitors(Computing):
+    __tablename__ = "monitors"
+    id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
+    brand = Column(String, index=True)
+    model = Column(String, index=True)
+    price = Column(Integer, index=True)
+    quantity = Column(Integer, index=True)
+    description = Column(String, index=True)
+    image = Column(String, index=True)
+    def __repr__(self):
+        return f"Monitors(id={self.id}, name={self.name}, brand={self.brand}, model={self.model}, price={self.price}, quantity={self.quantity}, description={self.description}, image={self.image})"
                                 class Printers(Computing):
                                     __tablename__ = "printers"
                                     id = Column(Integer, ForeignKey("computings.id"), primary_key=True)
