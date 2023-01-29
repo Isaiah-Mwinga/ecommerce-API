d7ab97317f4e86a469bcf3d09385e85af69413d3
Isaiah-Mwinga <motongori@marketforce360.com>
2023-01-29 21:52:59 +0300

    put and delete routes

--- app/Desktops/Desktops.py
+++ app/Desktops/Desktops.py
@@ -15,3 +15,26 @@ def create_Desktops(Desktops: schemas.Desktops, db: Session = Depends(get_db)):
 @router.get("/Desktops/all", status_code=status.HTTP_200_OK, response_model=list[schemas.Desktops])
 def get_all_Desktops(db: Session = Depends(get_db)):
     return crud.get_all_Desktops(db)
+
+@router.get("/Desktops/{Desktops_id}", status_code=status.HTTP_200_OK, response_model=schemas.Desktops)
+def get_Desktops_by_id(Desktops_id: int, db: Session = Depends(get_db)):
+    db_Desktops = crud.get_Desktops_by_id(Desktops_id, db)
+    if not db_Desktops:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Desktops not found")
+    return db_Desktops
+
+@router.put("/Desktops/{Desktops_id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Desktops)
+def update_Desktops(Desktops_id: int, Desktops: schemas.Desktops, db: Session = Depends(get_db)):
+    db_Desktops = crud.get_Desktops_by_id(Desktops_id, db)
+    if not db_Desktops:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Desktops not found")
+    return crud.update_Desktops(Desktops_id, Desktops, db)
+
+@router.delete("/Desktops/{Desktops_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_Desktops(Desktops_id: int, db: Session = Depends(get_db)):
+    db_Desktops = crud.get_Desktops_by_id(Desktops_id, db)
+    if not db_Desktops:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Desktops not found")
+    crud.delete_Desktops(Desktops_id, db)
+
+# Path: app/Computing/crud.py
