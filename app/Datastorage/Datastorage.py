b80d2051254a1192820e053cc07dc58dd3185fd7
Isaiah-Mwinga <motongori@marketforce360.com>
2023-02-01 22:29:28 +0300

    post router

--- app/Datastorage/Datastorage.py
+++ app/Datastorage/Datastorage.py
@@ -8,3 +8,9 @@ router = APIRouter(
     prefix="/Datastorage",
     tags=["Datastorage"],
 )
+
+@router.post("/Datastorage/",status_code=status.HTTP_201_CREATED, response_model=schemas.Datastorage)
+def create_Datastorage(Datastorage: schemas.Datastorage, db: Session = Depends(get_db)):
+    if crud.get_Datastorage_by_name(Datastorage.name, db):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Datastorage already exists")
+    return crud.create_Datastorage(Datastorage, db)
