1a784360d00d1a6427af839c4b6c6d1cfa16e1ad
Isaiah-Mwinga <motongori@marketforce360.com>
2023-02-01 22:30:23 +0300

    get endpoint

--- app/Datastorage/Datastorage.py
+++ app/Datastorage/Datastorage.py
@@ -14,3 +14,15 @@ def create_Datastorage(Datastorage: schemas.Datastorage, db: Session = Depends(g
     if crud.get_Datastorage_by_name(Datastorage.name, db):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Datastorage already exists")
     return crud.create_Datastorage(Datastorage, db)
+
+@router.get("/Datastorage/all", status_code=status.HTTP_200_OK, response_model=list[schemas.Datastorage])
+def get_all_Datastorage(db: Session = Depends(get_db)):
+    return crud.get_all_Datastorage(db)
+
+@router.get("/Datastorage/{Datastorage_id}", status_code=status.HTTP_200_OK, response_model=schemas.Datastorage)
+def get_Datastorage_by_id(Datastorage_id: int, db: Session = Depends(get_db)):
+    db_Datastorage = crud.get_Datastorage_by_id(Datastorage_id, db)
+    if not db_Datastorage:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Datastorage not found")
+    return db_Datastorage
+    
