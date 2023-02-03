02c50802be608132aaf52e62e4a687bfc48a12dd
Isaiah-Mwinga <motongori@marketforce360.com>
2023-02-03 12:25:51 +0300

    tests

--- app/Users/test_users.py
+++ app/Users/test_users.py
@@ -26,3 +26,27 @@ def test_create_user(client: TestClient):
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {'detail': 'Input body requires an "email" field.'}
+
+def test_update_user(client: TestClient):
+    # First, create a user
+    user_data = {
+        "email": "test@example.com",
+        "password": "secret"
+    }
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    user_id = response.json()["id"]
+
+    # Then, update the user
+    update_data = {
+        "email": "updated@example.com",
+        "password": "newsecret"
+    }
+    response = client.put(f"/users/{user_id}", json=update_data)
+    assert response.status_code == 200
+
+    # Finally, retrieve the updated user and check that the data is correct
+    response = client.get(f"/users/{user_id}")
+    assert response.status_code == 200
+    assert response.json()["email"] == "updated@example.com"
+    assert response.json()["password"] == "newsecret"
