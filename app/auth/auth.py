e8829636f1ec5889f5c934c48f086890573acecc
Isaiah-Mwinga <motongori@marketforce360.com>
2023-02-04 22:43:31 +0300

    refactor

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -22,7 +22,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
+    user = session.query(User).filter(User.email == username).first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
