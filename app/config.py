c52787f898e127a6855e6eb3d985d16f90c4518a
Isaiah-Mwinga <motongori@marketforce360.com>
2023-02-05 21:39:23 +0300

    config

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Settings(BaseSettings):
-    db_host: str = 'db'
-    db_user: str = 'myuser'
+    db_host: str = 'localhost'
+    db_user: str = 'postgres'
     db_password: str = 'root'
-    db_name: str = 'mydb'
+    db_name: str = 'item_db'
     test_db_name: str = 'testdb'
     redis_host: str = os.getenv('DOCKER_REDIS_HOST', 'localhost')
     redis_port: str = '6379'
